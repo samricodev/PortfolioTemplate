3f987b62ae499d7913e7f1ed9d64431062c43e0d
samricodev <samuel.isaac.rico.estrada@gmail.com>
2023-01-31 14:15:49 -0600

    remove the page  that avoid the correct deploying

--- pages/post.jsx
+++ pages/post.jsx
@@ -1,22 +1,22 @@
-import Layout from './layout'
-import { useRouter} from 'next/router'
-import { posts } from '@/profile'
-import Image from 'next/image'
+// import Layout from './layout'
+// import { useRouter} from 'next/router'
+// import { posts } from '@/profile'
+// import Image from 'next/image'
 
-function Post () {
+// function Post () {
 
-  const router = useRouter()
-  const post = posts.filter(post => post.title === router.query.title)[0]
+//   const router = useRouter()
+//   const post = posts.filter(post => post.title === router.query.title)[0]
 
-  return (
-    <Layout footer={false} title={post.title}>
-      <div className="text-center">
-        <h1>{post.title}</h1>
-        <Image className='img-fluid' src={post.imageURL} width={250} height={250} alt={post.title} />
-        <p>{post.content}</p>
-      </div>
-    </Layout>
-  )
-}
+//   return (
+//     <Layout footer={false} title={post.title}>
+//       <div className="text-center">
+//         <h1>{post.title}</h1>
+//         <Image className='img-fluid' src={post.imageURL} width={250} height={250} alt={post.title} />
+//         <p>{post.content}</p>
+//       </div>
+//     </Layout>
+//   )
+// }
 
-export default Post
+// export default Post
